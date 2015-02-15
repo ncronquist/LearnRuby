85a39beac39ba48eec4524b1dc6e1ee9e75210a0
ncronquist <nick@ncronquist.com>
2015-02-15 12:15:06 -0800

    LtP 2.5 Basic time calculations

---
+++ CalcTime.rb
@@ -0,0 +1,8 @@
+# Calculate the number of hours in a year
+puts 1 * 365 * 24
+
+# Calculate the number of minutes in a decade
+puts 10 * 365 * 24 * 60
+
+# Calcualate the authors age if his is 1,160 million seconds old
+puts 1160000000 / 60 / 60 / 24 / 365
