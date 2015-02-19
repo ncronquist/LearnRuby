54b76ada64ec32b25382f1f6aaad539e7caa7b44
ncronquist <nick@ncronquist.com>
2015-02-19 15:47:52 -0800

    Add coding examples for chapter 4

---
+++ chapter_4_variables_assignment/chapter_4.rb
@@ -0,0 +1,34 @@
+# Chapter 4 Code Exercises
+
+# 4.0 Variables
+my_string = '... you can say that again...'
+puts my_string # => ... you can say that again...
+puts my_string # => ... you can say that again...
+
+name = 'Anya Christina Emmanuella Jenkins Harris'
+puts 'My name is ' + name + '.' # => My name is Anya Christina Emmanuella Jenkins Harris.
+puts 'Wow! ' + name # => Wow! Anya Christina Emmanuella Jenkins Harris
+puts 'is a really long name!' # => is a really long name!
+
+composer = 'Mozart'
+puts composer + ' was "da bomb" in his day.' # => Mozart was "da bomb" in his day.
+
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally.' # => But I prefer Beethoven, personally.
+
+my_own_var = 'just another ' + 'string'
+puts my_own_var # => just another string
+
+my_own_var = 5 * (1+2)
+puts my_own_var # => 15
+
+var1 = 8
+var2 = var1
+puts var1 # => 8
+puts var2 # => 8
+
+puts '' # =>
+
+var1 = 'eight'
+puts var1 # => eight
+puts var2 # => 8
