5b3b2e61d01d3160c20f4ef2e190e0c31ac23ef4
ncronquist <nick@ncronquist.com>
2015-02-25 18:26:29 -0800

    Add chapter 6 notes

--- chapter_6_more_about_methods/chapter_6.rb
+++ chapter_6_more_about_methods/chapter_6.rb
@@ -93,3 +93,53 @@ puts('Chapter 1:'.ljust(left_col) + 'Getting Started'.ljust(center_col) + 'page 
 puts('Chapter 2:'.ljust(left_col) + 'Numbers'.ljust(center_col) + 'page  9'.rjust(right_col))
 puts('Chapter 3:'.ljust(left_col) + 'Letters'.ljust(center_col) + 'page  13'.rjust(right_col))
 puts ''
+
+# 6.4 More Arithmetic
+puts 5**2 # => 25
+puts 5**0.5 # => 2.23606...
+puts 7/3 # => 2
+puts 7%3 # => 1
+puts 365%7 # => 1
+puts ''
+
+puts (5-2).abs # => 3
+puts (2-5).abs # => 3
+puts ''
+
+#6.5 Random Numbers
+puts rand # => random float >= 0 and < 1
+puts rand # => ``
+puts rand # => ``
+puts (rand(100)) # => random int >= 0 and < 100
+puts (rand(100)) # => ``
+puts (rand(100)) # => ``
+puts (rand(1)) # => 0 because randome int >= 0 and < 1
+puts (rand(1)) # ``
+puts (rand(1)) # ``
+puts (rand(99999999999999999999999999999999999999)) # => random int >= 0
+puts('The weatherman said there is a')
+puts(rand(101).to_s + '% chance of rain,')
+puts('but you can never trust a weatherman.')
+
+# Using srand allows you to set a seed for the following rands
+# This will make the rands always produce the same random numbers
+srand 1976
+puts(rand(100)) # => 50
+puts(rand(100)) # => 21
+puts(rand(100)) # => 80
+puts(rand(100)) # => 15
+puts ''
+srand 1976
+puts(rand(100)) # => 50
+puts(rand(100)) # => 21
+puts(rand(100)) # => 80
+puts(rand(100)) # => 15
+puts ''
+
+# 6.6
+puts(Math::PI) # =>  3.1415...
+puts(Math::E) # => 2.71828...
+puts(Math.cos(Math::PI/3)) # => 0.500000...1
+puts(Math.tan(Math::PI/4)) # => 0.99999....
+puts(Math.log(Math::E**2)) # => 2.0
+puts((1 + Math.sqrt(5))/2) # => 1.6180...
